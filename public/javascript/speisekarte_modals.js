283a0f1c1a13d221dab66451f3d45f711681fa5e
FinnReimann <finnreimann@web.de>
2022-04-26 16:20:43 +0200

    speisekarte modals fertig

--- public/javascript/speisekarte_modals.js
+++ public/javascript/speisekarte_modals.js
@@ -1,41 +1,39 @@
 /* Pizzen Modal */
-let pizzen_open = document.querySelectorAll(".pizzen_open");
-let pizzen_close = document.querySelectorAll(".pizzen_close");
-let pizzen_container = document.querySelectorAll(".pizzen_container");
+const pizzen_open = document.querySelectorAll(".pizzen_open");
+const pizzen_close = document.querySelectorAll(".pizzen_close");
+const pizzen_container = document.querySelectorAll(".pizzen_container");
 
-for (var i = 0; i < pizzen_close.length; i++) {
+for (let i = 0; i < pizzen_close.length; i++) {
   pizzen_open[i].addEventListener("click", () => {
     pizzen_container[i].classList.add("pizza_show");
   });
+  pizzen_close[i].addEventListener("click", () => {
+    pizzen_container[i].classList.remove("pizza_show");
+  });
+  window.addEventListener("mouseup", (event) => {
+    if (event.target == pizzen_container[i]) {
+      pizzen_container[i].classList.remove("pizza_show");
+    }
+  });
 }
 
-for (var i = 0; i < pizzen_close.length; i++) {}
-pizzen_close[i].addEventListener("click", () => {
-  pizzen_container[i].classList.remove("pizza_show");
-});
-
-for (var i = 0; i < pizzen_close.length; i++) {}
-window.addEventListener("mouseup", (event) => {
-  if (event.target == pizzen_container[i]) {
-    pizzen_container[i].classList.remove("pizza_show");
-  }
-});
-
 /* Drinks Modal */
-const drinks_open = document.querySelector(".drinks_open");
-const drinks_close = document.querySelector(".drinks_close");
-const drinks_container = document.querySelector(".drinks_container");
+const drinks_open = document.querySelectorAll(".drinks_open");
+const drinks_close = document.querySelectorAll(".drinks_close");
+const drinks_container = document.querySelectorAll(".drinks_container");
 
-drinks_open.addEventListener("click", () => {
-  drinks_container.classList.add("drink_show");
-});
+for (let i = 0; i < drinks_close.length; i++) {
+  drinks_open[i].addEventListener("click", () => {
+    drinks_container[i].classList.add("drink_show");
+  });
 
-drinks_close.addEventListener("click", () => {
-  drinks_container.classList.remove("drink_show");
-});
+  drinks_close[i].addEventListener("click", () => {
+    drinks_container[i].classList.remove("drink_show");
+  });
 
-window.addEventListener("mouseup", (event) => {
-  if (event.target == drinks_container) {
-    drinks_container.classList.remove("drink_show");
-  }
-});
+  window.addEventListener("mouseup", (event) => {
+    if (event.target == drinks_container[i]) {
+      drinks_container[i].classList.remove("drink_show");
+    }
+  });
+}
